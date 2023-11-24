13e55a735ea8d185b4e3935201cf1913b01fa04b
KjennethObrice <kenneth.olibrice@gmail.com>
2023-11-23 21:31:29 -0500

    Added wheel locations on the robot for a mecanum drive with the shorter crossbars.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -10,10 +10,10 @@ public class Constants {
         public static final double MaxTranslationError = 0; // CHANGE THIS
         public static final double MaxRotationError = 0; // CHANGE THIS
         public static final Rotation2d AnglePIDTolerance = Rotation2d.fromDegrees(3.0);
-        public static final Translation2d FrontLeftMotorLocation = new Translation2d(0, 0);
-        public static final Translation2d FrontRightMotorLocation = new Translation2d(0, 0);
-        public static final Translation2d BackLeftMotorLocation = new Translation2d(0, 0);
-        public static final Translation2d BackRightMotorLocation = new Translation2d(0, 0);
+        public static final Translation2d FrontLeftMotorLocation = new Translation2d(0.178, 0.178);
+        public static final Translation2d FrontRightMotorLocation = new Translation2d(0.178, -0.178);
+        public static final Translation2d BackLeftMotorLocation = new Translation2d(-0.178, 0.178);
+        public static final Translation2d BackRightMotorLocation = new Translation2d(-0.178, -0.178);
         public static final double TranslationToleranceMeters = 0.03; // 3 millimeters of permissible error. Tune later if needed
     }
 
