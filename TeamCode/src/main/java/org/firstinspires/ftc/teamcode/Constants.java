27689e95c09cf3dd90ff41232e635bf37bbb30f0
KjennethObrice <kenneth.olibrice@gmail.com>
2023-11-26 10:02:40 -0500

    Switched the drive constants to reflect the larger chassis we will be using for testing on Monday/Tuesday.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -20,10 +20,10 @@ public class Constants {
         public static final double MaxRobotSpeedMetersPerSecond = 3;
         public static final double MaxAngularVeloityRadiansPerSecond = Math.PI;
         public static final Rotation2d AnglePIDTolerance = Rotation2d.fromDegrees(3.0); // Maximum 3 degree difference between set and actual
-        public static final Translation2d FrontLeftMotorLocation = new Translation2d(0.178, 0.178);
-        public static final Translation2d FrontRightMotorLocation = new Translation2d(0.178, -0.178);
-        public static final Translation2d BackLeftMotorLocation = new Translation2d(-0.178, 0.178);
-        public static final Translation2d BackRightMotorLocation = new Translation2d(-0.178, -0.178);
+        public static final Translation2d FrontLeftMotorLocation = new Translation2d(0.207, 0.168);
+        public static final Translation2d FrontRightMotorLocation = new Translation2d(0.207, -0.168);
+        public static final Translation2d BackLeftMotorLocation = new Translation2d(-0.207, 0.168);
+        public static final Translation2d BackRightMotorLocation = new Translation2d(-0.207, -0.168);
         public static final double TranslationToleranceMeters = 0.03; // 3 millimeters of permissible error. Tune later if needed
     }
 
