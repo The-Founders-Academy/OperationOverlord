25cb88a85f99beb5a4db838eca470305de10d95a
KjennethObrice <kenneth.olibrice@gmail.com>
2023-11-26 10:01:14 -0500

    Added some context for wheel constants in Constants.java.
    We will be using a shorter drivetrain width on the actual robot so the lifts don't push us over the 18in size limit. The part that will allow us to make the robot smaller won't actually be in until later this week though, so we will be testing with a wider robot and will need to use the 10 hole crossbar values. This should be fine, because our robot code--if it is written correctly--should work with any size robot so long as we measure the wheel distances correctly.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -4,12 +4,22 @@ import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
 
 public class Constants {
+
+    /*
+    Wheel center distances:
+    ----------------------
+    8 hole crossbar:
+    horizontal: 178 mm
+    vertical: 168 mm
+
+    10 hole crossbar (what we'll have on Monday/Tuesday):
+    horizontal: 207 mm
+    vertical: 168 mm
+     */
     public static class DrivetrainConstants {
         public static final double MaxRobotSpeedMetersPerSecond = 3;
         public static final double MaxAngularVeloityRadiansPerSecond = Math.PI;
-        public static final double MaxTranslationError = 0; // CHANGE THIS
-        public static final double MaxRotationError = 0; // CHANGE THIS
-        public static final Rotation2d AnglePIDTolerance = Rotation2d.fromDegrees(3.0);
+        public static final Rotation2d AnglePIDTolerance = Rotation2d.fromDegrees(3.0); // Maximum 3 degree difference between set and actual
         public static final Translation2d FrontLeftMotorLocation = new Translation2d(0.178, 0.178);
         public static final Translation2d FrontRightMotorLocation = new Translation2d(0.178, -0.178);
         public static final Translation2d BackLeftMotorLocation = new Translation2d(-0.178, 0.178);
@@ -17,11 +27,4 @@ public class Constants {
         public static final double TranslationToleranceMeters = 0.03; // 3 millimeters of permissible error. Tune later if needed
     }
 
-    public static class OdometryConstants {
-        public static final double TrackWidthInches = 0; // Distance between left odometry pod and right odometry pod
-        public static final double CenterOdometryPodOffsetInches = 0; // Distance between center of rotation and center odometry pod
-        public static final double OdometryPodTicksPerRevolution = 2000; // Obtained from the GoBILDA odometry pod webapge.
-        public static final double OdometryWheelDiameterInhes = 1.89; // Obtained from the GoBILDA odometry pod webapge.
-        public static final double DistancePerPulse = Math.PI * OdometryWheelDiameterInhes / OdometryPodTicksPerRevolution;
-    }
 }
