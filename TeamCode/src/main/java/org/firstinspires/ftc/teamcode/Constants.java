8af19038a418c9892593ce5cd2d664e595be2352
evanjducas <evan.j.ducas@gmail.com>
2023-12-05 16:00:56 -0500

    Fixed wheel pose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class Constants {
 
@@ -12,20 +14,30 @@ public class Constants {
     horizontal: 178 mm
     vertical: 168 mm
 
+
+
     10 hole crossbar (what we'll have on Monday/Tuesday):
     horizontal: 207 mm
     vertical: 168 mm
      */
+    @Config
     public static class DrivetrainConstants {
-        public static final double MaxRobotSpeedMetersPerSecond = 1.5; // Theoretical value from the strafer chassis product page
-        public static final double MaxAngularVeloityRadiansPerSecond = 2*Math.PI;
-        public static final Rotation2d AnglePIDTolerance = Rotation2d.fromDegrees(3.0); // Maximum 3 degree difference between set and actual
-        public static final Translation2d FrontLeftMotorLocation = new Translation2d(0.207, 0.168);
-        public static final Translation2d FrontRightMotorLocation = new Translation2d(0.207, -0.168);
-        public static final Translation2d BackLeftMotorLocation = new Translation2d(-0.207, 0.168);
-        public static final Translation2d BackRightMotorLocation = new Translation2d(-0.207, -0.168);
-        public static final double TranslationToleranceMeters = 0.03; // 3 millimeters of permissible error. Tune later if needed
-        public static final double DistancePerEncoderTick = 0.00056; // 0.56 mm per pulse
+        public static double MaxRobotSpeedMetersPerSecond = 1.5; // Theoretical value from the strafer chassis product page
+        public static double MaxAngularVeloityRadiansPerSecond = 2*Math.PI;
+        public static Rotation2d AnglePIDTolerance = Rotation2d.fromDegrees(3.0); // Maximum 3 degree difference between set and actual
+        public static Translation2d FrontLeftMotorLocation = new Translation2d(0.178, 0.168);
+        public static Translation2d FrontRightMotorLocation = new Translation2d(0.178, -0.168);
+        public static Translation2d BackLeftMotorLocation = new Translation2d(-0.178, 0.168);
+        public static Translation2d BackRightMotorLocation = new Translation2d(-0.178, -0.168);
+        public static double TranslationToleranceMeters = 0.03;
+
+        // 3 millimeters of permissible error. Tune later if needed
+        public static PIDFCoefficients XPIDF = new PIDFCoefficients(0.1, 0, 0, 0);
+        public static PIDFCoefficients YPIDF = new PIDFCoefficients(0.1, 0, 0, 0);
+        public static PIDFCoefficients AnglePIDF = new PIDFCoefficients(0.1, 0, 0, 0);
+
+
+        public static double DistancePerEncoderTick = 0.00056; // 0.56 mm per pulse
     }
 
 }
